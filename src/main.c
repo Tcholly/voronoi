61f10ee8f3e5359fcf32dc86fdc83a4c592305bb
Tcholly <roberti.gionata@protonmail.ch>
2023-07-29 18:22:22 +0200

    Added simple animation and various distance functions

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <float.h>
 #include <time.h>
+#include <assert.h>
 #include <raylib.h>
 
 #define SEED_RADIUS 5.0f
 #define SEED_COLOR (Color){33, 144, 171, 255}
-
 
 typedef struct
 {
@@ -18,14 +18,29 @@ typedef struct
 #define SEEDS_COUNT 10
 voronoi_seed seeds[SEEDS_COUNT] = {0};
 
-Image display;
+typedef char display_underlying_type;
+// expect less than 127 seeds
+display_underlying_type* display = NULL;
+int display_width = 0;
+int display_height = 0;
 
+typedef enum
+{
+	EUCLIDEAN_DISTANCE = 0,
+	MANHATTAN_DISTANCE,
+	CHEBYSHEV_DISTANCE,
+	MINKOWSKI_DISTANCE,
+
+	DISTANCE_TYPE_MAX
+} distance_type;
+static_assert(DISTANCE_TYPE_MAX == 4, "Remember to implement all distances types");
+
+distance_type current_distance_type = EUCLIDEAN_DISTANCE;
 float animation_distance = 0.0f;
 
 float random_float(float min, float max)
 {
 	int r = rand();
-	printf("%d\n", r);
 
 	return ((float)r / (float)RAND_MAX) * (max - min) + min;
 }
@@ -40,7 +55,35 @@ Color random_color()
 	return result;
 }
 
-float distance(Vector2 first, Vector2 second)
+void realloc_display(int width, int height)
+{
+	if (width == display_width && height == display_height)
+		goto reset_values;
+
+	if (display != NULL)
+		free(display);
+
+	display = (display_underlying_type*)malloc(width * height * sizeof(display_underlying_type));
+
+	display_width = width;
+	display_height = height;
+
+reset_values:
+	for (int i = 0; i < display_height * display_width; i++)
+		display[i] = -1;
+}
+
+int get_display_value(int x, int y)
+{
+	return display[y * display_width + x];
+}
+
+void set_display_value(int x, int y, int value)
+{
+	display[y * display_width + x] = value;
+}
+
+float euclidean_distance(Vector2 first, Vector2 second)
 {
 	Vector2 diff;
 	diff.x = second.x - first.x;
@@ -49,15 +92,58 @@ float distance(Vector2 first, Vector2 second)
 	return sqrtf(diff.x * diff.x + diff.y * diff.y);
 }
 
+float manhattan_distance(Vector2 first, Vector2 second)
+{
+	return fabsf(second.x - first.x) + fabsf(second.y - first.y);
+}
+
+float chebyshev_distance(Vector2 first, Vector2 second)
+{
+	float x_diff = fabsf(first.x - second.x);
+	float y_diff = fabsf(first.y - second.y);
+
+	return fmaxf(x_diff, y_diff);
+}
+
+
+#define MINKOWSKI_ORDER 4
+float minkowski_distance(Vector2 first, Vector2 second)
+{
+	return powf(powf(fabsf(first.x - second.x), MINKOWSKI_ORDER) + powf(fabsf(first.y - second.y), MINKOWSKI_ORDER), 1.0f / MINKOWSKI_ORDER);
+}
+
+float distance(Vector2 first, Vector2 second)
+{
+	switch (current_distance_type)
+	{
+		case EUCLIDEAN_DISTANCE:
+			return euclidean_distance(first, second);
+		case MANHATTAN_DISTANCE:
+			return manhattan_distance(first, second);
+		case CHEBYSHEV_DISTANCE:
+			return chebyshev_distance(first, second);
+		case MINKOWSKI_DISTANCE:
+			return minkowski_distance(first, second);
+
+		default:
+			printf("Unreachable: distance");
+			break;
+	}
+
+	return 0.0f;
+}
+
+
 void voronoi(void)
 {
-	for (int y = 0; y < display.height ; y++)
+	for (int y = 0; y < display_height ; y++)
 	{
-		for (int x = 0; x < display.width; x++)
+		for (int x = 0; x < display_width; x++)
 		{
-			Color image_pixel = GetImageColor(display, x, y);
-			if (image_pixel.r != 255 || image_pixel.g != 255 || image_pixel.b != 255)
+			int pixel_index = get_display_value(x, y);
+			if (pixel_index >= 0)
 				continue;
+
 			float min_distance = FLT_MAX;
 			int seed_index = 0;
 			for (int i = 0; i < SEEDS_COUNT; i++)
@@ -71,10 +157,9 @@ void voronoi(void)
 			}
 
 			if (min_distance < animation_distance)
-				ImageDrawPixel(&display, x, y, seeds[seed_index].color);
+				set_display_value(x, y, seed_index);
 		}
 	}
-
 }
 
 int main(void)
@@ -84,7 +169,7 @@ int main(void)
 	InitWindow(800, 480, "Voronoi diagram");
 	SetTargetFPS(60);
 
-	display = GenImageColor(800, 480, WHITE);
+	realloc_display(800, 480);
 
 	while (!WindowShouldClose())
 	{
@@ -97,27 +182,66 @@ int main(void)
 				seeds[i].color = random_color();
 			}
 
-			UnloadImage(display);
-			display = GenImageColor(GetScreenWidth(), GetScreenHeight(), WHITE);
+			realloc_display(GetScreenWidth(), GetScreenHeight());
+			animation_distance = 0.0f;
+		}
+
+		if (IsKeyPressed(KEY_SPACE))
+		{
+			current_distance_type += 1;
+			if (current_distance_type == DISTANCE_TYPE_MAX)
+				current_distance_type = 0;
+			realloc_display(GetScreenWidth(), GetScreenHeight());
 			animation_distance = 0.0f;
 		}
 
 		animation_distance += GetFrameTime() * 100;
 		voronoi();
 
-		Texture2D display_texture = LoadTextureFromImage(display);
 		BeginDrawing();
 		ClearBackground(WHITE);
 		
-		DrawTexture(display_texture, 0, 0, WHITE);
+		for (int y = 0; y < display_height; y++)
+		{
+			for (int x = 0; x < display_width; x++)
+			{
+				int index = get_display_value(x, y);
+				if (index >= 0)
+					DrawPixel(x, y, seeds[index].color);
+			}
+		}
 
 		for (int i = 0; i < SEEDS_COUNT; i++)
 			DrawCircleV(seeds[i].position, SEED_RADIUS, SEED_COLOR);
 
+		const char* distance_type_text;
+
+		switch (current_distance_type)
+		{
+			case EUCLIDEAN_DISTANCE:
+				distance_type_text = "Euclidean distance";
+				break;
+			case MANHATTAN_DISTANCE:
+				distance_type_text = "Manhattan distance";
+				break;
+			case CHEBYSHEV_DISTANCE:
+				distance_type_text = "Chebyshev distance";
+				break;
+			case MINKOWSKI_DISTANCE:
+				distance_type_text = "Minkowski distance";
+				break;
+
+			default:
+				distance_type_text = "Unknown distance";
+				break;
+		}
+
+		DrawText(distance_type_text, 10, 10, 20, LIGHTGRAY);
+
 		EndDrawing();
-		UnloadTexture(display_texture);
 	}
 
+	free(display);
 	CloseWindow();
 	return 0;
 }
